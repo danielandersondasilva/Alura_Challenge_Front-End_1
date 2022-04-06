c6ee30b1f983879afed54a3f0efb206f0b5f690f
Daniel Anderson <danielandersondasilva@gmail.com>
2022-04-06 13:17:32 -0300

    implementando o menu

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ function aplicaHighlight() {
 botao.addEventListener('click', () => {
     aplicaHighlight()
 })
+
+
+
+
+
+
+const comunidade = document.querySelector('.menu_nav img:last-child');
+const menuEsquerdo = document.querySelector('.menu_esquerdo');
+
+function vaiPraComunidade() {
+    if (comunidade.getAttribute('src') == '/img/Vector.svg') {
+        comunidade.setAttribute('src','/img/fechar.svg');
+        menuEsquerdo.classList.add('ativado');
+        
+        
+    }else{
+        comunidade.setAttribute('src','/img/Vector.svg')
+        menuEsquerdo.classList.remove('ativado');
+        
+    }
+}
+
+comunidade.addEventListener('click', vaiPraComunidade)
